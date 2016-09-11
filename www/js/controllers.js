0eb6d3c0f50facf7bbad00d9ca3b254958dad017
dianaman <diana.man.91@gmail.com>
2016-09-11 01:36:22 -0300

    intento fallido de nativeAudio y media

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,27 +2,60 @@ angular.module('starter.controllers', ['ngCordova'])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, $cordovaVibration) {
-  $scope.elegirCorrecto = function(){
+.controller('ChatsCtrl', function($scope, $rootScope, $state, $ionicModal, $ionicPlatform, $cordovaVibration, $cordovaNativeAudio) {
+  var mediaWin = null;
+  var mediaLoose = null;
+  
 
-    try{
-      $cordovaVibration.vibrate(100);
+  try{
+    $ionicPlatform.ready(function(){
+      /*
+      mediaWin = $cordovaMedia.newMedia("/sounds/correcto.mp3", null, null, null);
+      mediaLoose = $cordovaMedia.newMedia("/sounds/incorrecto.mp3", null,null, null);
+
+      mediaWin.then(function(){
+        alert("success");
+      }, function(){
+        alert("error");
+      });
+
+      mediaLoose.then(function(){
+        alert("success");
+      }, function(){
+        alert("error");
+      });*/
+      
+      $cordovaNativeAudio
+        .preloadSimple('loose', 'sounds/incorrecto.mp3');
+
+    })
+
+    $scope.elegirCorrecto = function(){
+
+      try{
+        //mediaWin.play();
+        $cordovaVibration.vibrate(100);
+      }
+      catch(err){
+        alert(err.message);
+      }
     }
-    catch(err){
-      console.log(err.message);
-    }
+
+    $scope.elegirIncorrecto = function(){
+      try{
+        $cordovaVibration.vibrate([100, 100, 100]);
+        $cordovaNativeAudio.play('loose');
+        
+      }
+      catch(err){
+        alert(err.message);
+      }
+    }  
+
+  } catch(error){
+    alert(error);
   }
-
-  $scope.elegirIncorrecto = function(){
-    try{
-      $cordovaVibration.vibrate([100, 100, 100]);
-    }
-    catch(err){
-      console.log(err.message);
-    }
-  }  
 })
-
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
 })
